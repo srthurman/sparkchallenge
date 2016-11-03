6688dd15cd82d9ccc1b2890a8d9d1b1b04c3fa1b
Sara Thurman <srthurman@gmail.com>
2016-11-03 13:57:22 -0400

    Basic arithmetic functions

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# arithmatic functions
+
+def add(x, y):
+   """This function adds two numbers"""
+   return x + y
+
+def subtract(x, y):
+   """This function subtracts two numbers"""
+   return x - y
+
+def multiply(x, y):
+   """This function multiplies two numbers"""
+   return x * y
+
+def divide(x, y):
+   """This function divides two numbers"""
+   return x / y
+
